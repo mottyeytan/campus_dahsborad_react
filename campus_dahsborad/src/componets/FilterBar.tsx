a095d0e7f3647a934e7c57e310e6f21d8588c853
שם המשתמש שלך <your-email@example.com>
2025-08-28 16:07:43 +0300

    filter_bar

---
+++ campus_dahsborad/src/componets/FilterBar.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import '../componets.css/FilterBar.css'
+import type {MouseEvent } from 'react'
+
+export default function FilterBar(){
+
+    const [isActive, setIsActive] = useState(false)
+    const [view, setView] = useState('GRID')
+    const [search, setSearch] = useState('')
+
+
+    function ToggleActive(){
+        setIsActive(!isActive)
+
+    }
+
+    function LayoutView(e: MouseEvent<HTMLButtonElement>){
+        e.preventDefault()
+        if(e.currentTarget.classList.contains('grid-view')){
+            setView('GRID')
+            console.log('GRID')
+        } else if(e.currentTarget.classList.contains('list-view')){
+            setView('LIST')
+            console.log('LIST')
+        }
+
+    }
+
+    function SearchEvent(e: MouseEvent<HTMLInputElement>){
+        e.preventDefault()
+        setSearch(e.currentTarget.value)
+        console.log(search)
+    }
+
+
+    return (
+        <div className='filter-bar'>
+            <input type='checkbox' id='toggle-button' onClick={ToggleActive} /> 
+            <label htmlFor='toggle-button' className='input-label-toggle' >Show only active members</label>
+            
+            <div className="switch-layout">
+            <button className= "grid-view"  onClick={LayoutView} >GRID VIEW </button>
+            <button className= "list-view" onClick={LayoutView}>LIST VIEW </button>
+
+            </div>
+
+
+            <div className= "search-bar">
+                <input type='text' placeholder='Search for an Event' onChange={SearchEvent} />
+            </div>
+
+        </div>
+    )
+}
